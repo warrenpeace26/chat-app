4e2d42c0e8e7afc2d34c5f3025cae5bbaf6b2276
Utkarsh Kumar <8460131930lucky@gmail.com>
2025-06-28 21:07:27 +0530

    Fix: correct import path for authContext.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, } from 'react-router-dom'
-import { AuthProvider } from './context/authContext.js'
-import { ChatProvider } from './context/chatContext.js'
+import { AuthProvider } from './context/authContext.jsx'
+import { ChatProvider } from './context/chatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
